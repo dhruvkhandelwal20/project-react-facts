76b07962c4bffa6fb47cab930610c6d7efb6c758
Dhruv Khandelwal <dhruvkhandelwal39@gmail.com>
2023-02-05 13:18:07 +0530

    First Commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav>
+      <img src="../../public/react-icon-small.png" className="nav-icon" />
+      <h2 className="nav-logo-title">ReactFacts</h2>
+      <h3 className="nav-title">React Course - First Project</h3>
+    </nav>
+  );
+}
+
+export default Navbar;
